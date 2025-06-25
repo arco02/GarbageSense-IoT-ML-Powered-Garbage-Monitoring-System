8f358b27d290b79100f1ce325cdfa5df7493648c
Arco <178926098+arco02@users.noreply.github.com>
2025-06-26 04:53:56 +0530

    Update flask_server.py

--- src/services/flask_server.py
+++ src/services/flask_server.py
@@ -4,8 +4,11 @@ import json
 import threading
 import joblib
 import paho.mqtt.client as mqtt
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 DATA_PATH = "data/external/latest_data.json" 
 
 
